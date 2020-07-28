486a0e81eadd7f5cfa923c023076b2eb7461dad9
Karol Rechman <39775135+KarolRechman@users.noreply.github.com>
2020-07-28 10:08:04 +0200

    Add files via upload

---
+++ Models/Translation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewOPAL.Models
+{
+    public class Translation
+    {
+    }
+    public class Words
+    {
+        [Column(Order = 1)]
+        public int Id { get; set; }
+        [Column(Order = 2)]
+        public string Text { get; set; }
+    }
+    public class Language
+    {
+        public int IdLanguage { get; set; }
+        public string EnglishName { get; set; }
+        public string NativeName { get; set; }
+        public string ISOCode { get; set; }
+    }
+
+
+}
