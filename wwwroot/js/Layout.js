0ff048f5cc346eb64acce2729035244e93878fe5
Karol Rechman <39775135+KarolRechman@users.noreply.github.com>
2020-07-28 10:41:35 +0200

    Add files via upload

---
+++ wwwroot/js/Layout.js
@@ -0,0 +1,77 @@
+﻿//history.replaceState(window.location.href, null, window.location.pathname);
+import { SideNav, MenuSession, LeftNav } from './Modules/MenuModule.js';
+import { HttpRequest } from './Modules/AxiosModule.js';
+import './Translations.js';
+
+const Main = $('main');
+const Theme = sessionStorage.getItem("data-theme");
+const ModeCheckbox = $("#MaterialToggleBlue");
+const wrapper = $(".wrapper");
+const ScrollingNav = $(".scrolling-navbar");
+
+let Table = $('table');
+
+const RightSideNav = new SideNav(document.getElementById("RightSidenav"), $("#sidenavRight"), $("#closebtnLeft"), wrapper, "SideNavRight");
+RightSideNav.openTrigger();
+RightSideNav.closeTrigger();
+
+const LeftSideNav = new LeftNav(document.getElementById("LeftSidenav"), $("#sidenavLeft"), $("#closebtnRight"), wrapper, "SideNavLeft");
+LeftSideNav.openTrigger();
+LeftSideNav.closeTrigger();
+LeftSideNav.anchorListener();
+LeftSideNav.CompanySelectListener();
+
+
+if (Theme == 1) {
+    Main.attr('data-theme', 'dark');
+    ModeCheckbox.prop("checked", true);
+} else {
+    Main.attr('data-theme', 'light');
+    ModeCheckbox.prop("checked", false);
+}
+
+$(window).bind("pageshow", function () {
+    $('input.form-control').each(function () {
+        this.value = "";
+    });
+    $('input').blur()
+});
+
+$(document).ready(function () {
+    ModeCheckbox.click(function () {
+        if (this.checked) {
+            sessionStorage.setItem("data-theme", 1);
+            Main.attr('data-theme', 'dark');
+            MenuSession("Post", 'data-theme', 1);
+            Table.addClass("table-dark");
+            Table.parent().addClass("special-color-dark");
+            Table.parent().parent().addClass("special-color-dark");
+        } else {
+            Main.attr('data-theme', 'light');
+            sessionStorage.setItem("data-theme", 0);
+            MenuSession("Put", 'data-theme', 0);
+            Table.removeClass("table-dark");
+            Table.parent().removeClass("special-color-dark");
+            Table.parent().parent().removeClass("special-color-dark");
+        }
+    });
+    $('#CompanySelect').selectpicker();
+    let IdCompany = sessionStorage.getItem("MenuCompany");
+    if (IdCompany != $("#CompanySelect").selectpicker('val')) {
+        MenuSession("Get", "MenuCompany").then(data => {
+            $("#CompanySelect").selectpicker('val', data);
+            console.log(data);
+        });
+    }
+});
+
+Main.scroll(function () {
+    if (this.scrollTop > 8) {
+        ScrollingNav.addClass("top-nav-collapse");
+    } else {
+        ScrollingNav.removeClass("top-nav-collapse");
+    }
+});
+
+
+
---
+++ wwwroot/js/Translations.js
@@ -0,0 +1,107 @@
+﻿import { HttpRequest } from './Modules/AxiosModule.js';
+var url = "api/Language/";
+sessionStorage.setItem("language", $("#Language").val());
+
+
+var pathname = window.location.pathname;
+var split = pathname.split("/");
+var PageName = split[3];
+if (PageName == undefined) {
+    PageName = "Login";
+}
+//var LanguageSettings = sessionStorage.getItem("language");
+//if (LanguageSettings == null) {
+//    sessionStorage.setItem("language", $("#Language").val());
+//}
+//const Loader = $(".loader");
+
+export function GetWordTranslation(word) {
+    let LanguageId = sessionStorage.getItem("language");
+    if (LanguageId == null || LanguageId == 1) {
+        return new Promise((resolve, reject) => {
+            resolve(word);
+        });
+    } else {
+        let params = {
+            word: word
+        }
+        return HttpRequest.get(url + "GetWord/" + LanguageId, { params }).then(function (response) {
+            return response.data;
+        });
+    }
+}
+
+export function GetTxtById(IdTxt, IdLanguage) {
+    let params = {
+        IdTxt: IdTxt
+    }
+    return HttpRequest.get(url + "GetTxtById/" + IdLanguage, { params }).then(function (response) {
+        return response.data;
+    });
+}
+
+$(document).ready(function () {
+    var Language = $("#Language");
+    var Words = [];
+
+    Language.change(function () {
+        let LanguageId = Language.val();
+        let LanguageSettings = sessionStorage.getItem("language");
+        let storageKey = PageName + "Words" + LanguageId.toString();
+        let wantedKey = PageName + "Words" + LanguageSettings.toString();
+        var TranslationElements = $(".Translation");
+
+        var stored = sessionStorage.getItem(storageKey);
+        if (stored) {
+            Words = JSON.parse(stored);
+            SetWords(TranslationElements, Words, LanguageId);
+            SetLanguageSession(LanguageId);
+        }
+        else {
+            if (LanguageId != LanguageSettings) {
+                Words = [];
+                TranslationElements.each(function (index) {
+                    Words.push({
+                        id: (index + 1).toString(),
+                        text: this.textContent.trim()
+                    });
+                    index += 1;
+                });
+                sessionStorage.setItem(wantedKey, JSON.stringify(Words));
+
+                HttpRequest.post(url + this.value, Words).then(function (response) {
+                    SetWords(TranslationElements, response.data, LanguageId);
+                    sessionStorage.setItem(storageKey, JSON.stringify(response.data));
+                }).catch(function (error) {
+                    //error modal !!!!!!
+                    console.log(error);
+                });
+            }
+        }
+    });
+});
+
+function SetWords(TranslationElements, Words, LanguageId) {
+    TranslationElements.hide();
+    let indexes = [];
+    for (var j = 0; j < Words.length; j++) {
+
+        TranslationElements.each(function (index) {
+            let check = indexes.includes(index);
+            if (check == false && j == index) {
+                this.textContent = Words[j].text;
+                indexes.push(index);
+                return false;
+            }
+        });
+    }
+    TranslationElements.fadeIn('slow');
+    sessionStorage.setItem("language", LanguageId);
+    Words = [];
+}
+
+function SetLanguageSession(LanguageId) {
+    HttpRequest.put(url + LanguageId);
+}
+
+
