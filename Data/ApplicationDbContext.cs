0120d9f7596fb139043062674cbd0c4ce7e6d3da
Karol Rechman <39775135+KarolRechman@users.noreply.github.com>
2020-07-28 10:05:25 +0200

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using NewOPAL.Models;
+
+namespace NewOPAL.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<OpalUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+           modelBuilder.Entity<OpalUser>().ToTable("Users", "OPAL");
+        }
+
+    }
+}
