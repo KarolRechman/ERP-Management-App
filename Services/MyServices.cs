9409fbf8f63738c800a78dc525cacc3e66f9347f
Karol Rechman <39775135+KarolRechman@users.noreply.github.com>
2020-07-28 10:23:25 +0200

    Add files via upload

---
+++ Services/MyServices.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NewOPAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewOPAL.Services
+{
+    public static class MyServices
+    {
+        public static IServiceCollection RegisterMyServices(this IServiceCollection services)
+        {
+            return services
+                    .AddTransient<IDataAccess, DataAccess>()
+                    .AddTransient<ITranslationService, TranslationService>()
+                    .AddTransient<IMenu, Menu>()
+                    .AddTransient<IHelpService, HelpService>()
+                    .AddTransient<ILocationsService, LocationsService>()
+                    .AddTransient<ICacheService, CacheService>()
+                    .AddTransient<ICustomerService, CustomerService>()
+                    .AddTransient<IPartsService, PartsService>()
+                    .AddTransient<IConnectionFactory, SqlConnectionFactory>();
+        }
+    }
+}
